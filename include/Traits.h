644daacf0a2d1f4bea25cddf1613959be475bcbc
ethandedalus <ethandedalus@gmail.com>
2026-01-29 04:23:14 -0500

    Remove unused traits

--- include/Traits.h
+++ include/Traits.h
@@ -3,8 +3,6 @@
 #include <format>
 #include <type_traits>
 
-#include "Types.h"
-
 template <typename T>
 concept Copy = std::is_trivially_copyable_v<T>;
 
@@ -13,24 +11,3 @@ concept Display =
     requires(const T& t) {
       std::format("{}", t);
     };
-
-template <typename G, typename T, usize ROWS, usize COLS>
-concept Grid2D =
-    requires(G& g, const G& cg, usize i, usize j, usize row, usize col) {
-      { g[i, j] } -> std::same_as<T&>;
-      { cg[i, j] } -> std::same_as<const T&>;
-      // { cg.GetRows() } -> std::same_as<usize>;
-      // { cg.GetCols() } -> std::same_as<usize>;
-      { cg.GetRow(row) } -> std::same_as<std::array<T, COLS>>;
-      { cg.GetCol(col) } -> std::same_as<std::array<T, ROWS>>;
-    };
-
-// constexpr inline auto GetRows() const noexcept -> usize { return ROWS; }
-// constexpr inline auto GetColumns() const noexcept -> usize { return COLS; }
-//
-// constexpr auto GetRow(usize row) const noexcept -> std::array<T, COLS> {
-//   std::array<T, COLS> out{};
-//   std::copy_n(m_inner.data() + row * COLS, COLS, out.begin());
-//
-//   return out;
-// }
